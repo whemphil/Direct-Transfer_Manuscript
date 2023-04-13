ed4ff0bbe51bac66bb8b183ae53bdf1222c4b01f
Wayne Hemphill <wayne.hemphill@colorado.edu>
2023-04-13 11:31:24 -0600

    Add files via upload

--- SMB-single-label_Final-Analysis.R
+++ SMB-single-label_Final-Analysis.R
@@ -1,54 +1,151 @@
-
 rm(list = ls())
 
-setwd('~/path/to/file/')
+setwd('/path/to/files/0uM/200ms/')
 
-load(file = 'no-decoy_rep-1_refine.particles.RData')
-null300.RT=residence.data$Residence
-load(file = 'no-decoy_rep-2_refine.particles.RData')
-null150.RT=residence.data$Residence
-load(file = 'high-decoy_rep-1_refine.particles.RData')
-decoy300.RT=residence.data$Residence
-load(file = 'high-decoy_rep-2_refine.particles.RData')
-decoy150.RT=residence.data$Residence
+load(file = 'r1/Refined-Particle_Data.RData')
+e1.0uM.200=residence.data$Residence
+load(file = 'r2/Refined-Particle_Data.RData')
+e1.0uM.200=c(e1.0uM.200,residence.data$Residence)
+load(file = 'r3/Refined-Particle_Data.RData')
+e1.0uM.200=c(e1.0uM.200,residence.data$Residence)
+load(file = 'r4/Refined-Particle_Data.RData')
+e1.0uM.200=c(e1.0uM.200,residence.data$Residence)
+example.trace=refined.particle.trace.rolls[,1]
+ex.time=seq(0.2,1487*0.2,0.2)/60
 
-null300.fit=density(null300.RT)
-null150.fit=density(null150.RT)
-decoy300.fit=density(decoy300.RT)
-decoy150.fit=density(decoy150.RT)
-par(fig = c(0,1,0.6,1))
-plot(null300.fit$x,null300.fit$y/max(null300.fit$y),type='l',xlab = 'Residence Time (s)',ylab = 'Relative Likelihood',main = expression('Residence Time Distributions: TREX1 + [Cy5]d(N)'[5]*' + d(N)'[5]),col='blue',ylim = c(0,1),xlim = c(0,400),cex.main=2)
-lines(null150.fit$x,null150.fit$y/max(null150.fit$y),col='blue')
-lines(decoy300.fit$x,decoy300.fit$y/max(decoy300.fit$y),col='red')
-lines(decoy150.fit$x,decoy150.fit$y/max(decoy150.fit$y),col='red')
-legend('topright',legend = c('Decoy = 0 µM','Decoy = 10 µM'),fill = c('blue','red'),col= c('blue','red'),cex = 1.5)
-par(fig = c(0,1,0.4,0.7),new=TRUE)
-boxplot(null300.RT,null150.RT,decoy300.RT,decoy150.RT,
-        at = c(4:1),
-        names = c("", "", "", ""),
+setwd('~/path/to/files/0uM/500ms/')
+
+load(file = 'r1/Refined-Particle_Data.RData')
+e1.0uM.500=residence.data$Residence
+load(file = 'r2/Refined-Particle_Data.RData')
+e1.0uM.500=c(e1.0uM.500,residence.data$Residence)
+load(file = 'r3/Refined-Particle_Data.RData')
+e1.0uM.500=c(e1.0uM.500,residence.data$Residence)
+load(file = 'r4/Refined-Particle_Data.RData')
+e1.0uM.500=c(e1.0uM.500,residence.data$Residence)
+
+setwd('/path/to/files/10uM/200ms/')
+
+load(file = 'r1/Refined-Particle_Data.RData')
+e1.10uM.200=residence.data$Residence
+load(file = 'r2/Refined-Particle_Data.RData')
+e1.10uM.200=c(e1.10uM.200,residence.data$Residence)
+load(file = 'r3/Refined-Particle_Data.RData')
+e1.10uM.200=c(e1.10uM.200,residence.data$Residence)
+load(file = 'r4/Refined-Particle_Data.RData')
+e1.10uM.200=c(e1.10uM.200,residence.data$Residence)
+
+setwd('/path/to/files/10uM/500ms/')
+
+load(file = 'r1/Refined-Particle_Data.RData')
+e1.10uM.500=residence.data$Residence
+load(file = 'r2/Refined-Particle_Data.RData')
+e1.10uM.500=c(e1.10uM.500,residence.data$Residence)
+load(file = 'r3/Refined-Particle_Data.RData')
+e1.10uM.500=c(e1.10uM.500,residence.data$Residence)
+load(file = 'r4/Refined-Particle_Data.RData')
+e1.10uM.500=c(e1.10uM.500,residence.data$Residence)
+
+setwd('/path/to/files/0uM/200ms/')
+
+load(file = 'r1/Refined-Particle_Data.RData')
+e2.0uM.200=residence.data$Residence
+load(file = 'r2/Refined-Particle_Data.RData')
+e2.0uM.200=c(e2.0uM.200,residence.data$Residence)
+load(file = 'r3/Refined-Particle_Data.RData')
+e2.0uM.200=c(e2.0uM.200,residence.data$Residence)
+load(file = 'r4/Refined-Particle_Data.RData')
+e2.0uM.200=c(e2.0uM.200,residence.data$Residence)
+
+setwd('/path/to/files/0uM/500ms/')
+
+load(file = 'r1/Refined-Particle_Data.RData')
+e2.0uM.500=residence.data$Residence
+load(file = 'r2/Refined-Particle_Data.RData')
+e2.0uM.500=c(e2.0uM.500,residence.data$Residence)
+load(file = 'r3/Refined-Particle_Data.RData')
+e2.0uM.500=c(e2.0uM.500,residence.data$Residence)
+load(file = 'r4/Refined-Particle_Data.RData')
+e2.0uM.500=c(e2.0uM.500,residence.data$Residence)
+
+setwd('/path/to/files/10uM/200ms/')
+
+load(file = 'r1/Refined-Particle_Data.RData')
+e2.10uM.200=residence.data$Residence
+load(file = 'r2/Refined-Particle_Data.RData')
+e2.10uM.200=c(e2.10uM.200,residence.data$Residence)
+load(file = 'r3/Refined-Particle_Data.RData')
+e2.10uM.200=c(e2.10uM.200,residence.data$Residence)
+load(file = 'r4/Refined-Particle_Data.RData')
+e2.10uM.200=c(e2.10uM.200,residence.data$Residence)
+
+setwd('/path/to/files/10uM/500ms/')
+
+load(file = 'r1/Refined-Particle_Data.RData')
+e2.10uM.500=residence.data$Residence
+load(file = 'r2/Refined-Particle_Data.RData')
+e2.10uM.500=c(e2.10uM.500,residence.data$Residence)
+load(file = 'r3/Refined-Particle_Data.RData')
+e2.10uM.500=c(e2.10uM.500,residence.data$Residence)
+load(file = 'r4/Refined-Particle_Data.RData')
+e2.10uM.500=c(e2.10uM.500,residence.data$Residence)
+
+
+par(fig = c(0.01,0.4,0.6,1))
+usable=(ex.time>=2 & ex.time<=4)
+event=c(808/300,876/300)
+plot(ex.time[usable],example.trace[usable],type = 'l',col='grey',main = 'Representative Particle Trace',xlab = 'Time (min)',ylab = 'Signal (A.U.)',cex.lab=1.4,cex.main=1.4,cex.axis=1.4)
+lines(c(0,event[1]),rep(0,times=2),col='black',lwd=3)
+lines(event,rep(7,times=2),col='green',lwd=3)
+lines(c(event[2],5),rep(0,times=2),col='black',lwd=3)
+legend('topright',legend = c('Bound','Unbound'),col = c('green','black'),fill = c('green','black'),cex=1)
+arrows(x0 = 3,x1 = 5.6,y0 = 12e3,y1 = 12e3,code = 3,lwd = 1,angle = 90,length = 0.15)
+mtext(expression(tau*' = 13.6 s'),side = 3,line = -10.5,at = 2.4,cex = 1.3)
+
+
+par(fig = c(0.01,1,0.205,0.6),new=TRUE)
+fit.0=MASS::fitdistr(c(e1.0uM.200,e1.0uM.500,e2.0uM.200,e2.0uM.500),densfun = 'exponential')
+fit.0sd=sd(c((MASS::fitdistr(c(e1.0uM.200),densfun = 'exponential'))$estimate,(MASS::fitdistr(c(e1.0uM.500),densfun = 'exponential'))$estimate,(MASS::fitdistr(c(e2.0uM.200),densfun = 'exponential'))$estimate,(MASS::fitdistr(c(e2.0uM.500),densfun = 'exponential'))$estimate))
+hist.0=hist(c(e1.0uM.200,e1.0uM.500,e2.0uM.200,e2.0uM.500),plot = FALSE,breaks = 20)
+fit.10=MASS::fitdistr(c(e1.10uM.200,e1.10uM.500,e2.10uM.200,e2.10uM.500),densfun = 'exponential')
+fit.10sd=sd(c((MASS::fitdistr(c(e1.10uM.200),densfun = 'exponential'))$estimate,(MASS::fitdistr(c(e1.10uM.500),densfun = 'exponential'))$estimate,(MASS::fitdistr(c(e2.10uM.200),densfun = 'exponential'))$estimate,(MASS::fitdistr(c(e2.10uM.500),densfun = 'exponential'))$estimate))
+hist.10=hist(c(e1.10uM.200,e1.10uM.500,e2.10uM.200,e2.10uM.500),plot = FALSE,breaks = 20)
+plot(hist.0$breaks,c(hist.0$counts,hist.0$counts[length(hist.0$counts)])/max(hist.0$counts),type ='s',lwd=1,col='blue',xlab = 'Residence Time (s)',ylab = 'Relative Likelihood',main = expression('Residence Time Distributions: TREX1 + [Cy5]d(N)'[5]*' + d(N)'[5]),ylim=0:1,xlim = c(0,100),cex.main=2,cex.lab=1.5,cex.axis=1.5)
+lines(hist.10$breaks,c(hist.10$counts,hist.10$counts[length(hist.10$counts)])/max(hist.10$counts),type='s',lwd=1,col='red')
+lines(seq(0,300,0.1),dexp(seq(0,300,0.1),rate = fit.10$estimate)/max(dexp(seq(0,300,0.1),rate = fit.10$estimate)),col='red',lwd=5)
+lines(seq(0,300,0.1),dexp(seq(0,300,0.1),rate = fit.0$estimate)/max(dexp(seq(0,300,0.1),rate = fit.0$estimate)),col='blue',lwd=5)
+legend('topright',legend = c('Competitor = 0 µM','Competitor = 10 µM'),fill = c('blue','red'),col= c('blue','red'),cex = 1.5)
+mtext(paste0('n = ',c(length(c(e1.0uM.200,e1.0uM.500,e2.0uM.200,e2.0uM.500)),length(c(e1.10uM.200,e1.10uM.500,e2.10uM.200,e2.10uM.500)))),side = 3,line = c(-2,-4),at = 60,cex = 2,col = c('blue','red'))
+
+par(fig = c(0.01,1,0,0.3),new=TRUE)
+boxplot(e1.0uM.200,e1.0uM.500,e2.0uM.200,e2.0uM.500,e1.10uM.200,e1.10uM.500,e2.10uM.200,e2.10uM.500,
+        at = c(4:-3),
+        names = paste0(rep(rep(c('A-','B-'),each=2),times=2),rep(c('0.2s','0.5s'),times=4)),
         las = 2,
-        col = c("blue","blue","red","red"),
-        border = c("blue","blue","red","red"),
+        col = rep(c('blue','red'),each=4),
+        border = rep(c('blue','red'),each=4),
         range=0,
         varwidth = TRUE,
         horizontal = TRUE,
         notch = TRUE,
         xaxt='n',
         yaxt='n',
-        ylim=c(0,400),
+        ylim=c(0,100),
         axes=FALSE
 )
-mtext(c('Rep-1','Rep-2','Rep-1','Rep-2'),side = 1,line = rev(c(-1.5,-2.9,-4.5,-5.9)),at = -25,cex = 1.5,col = rev(c('red','red','blue','blue')))
-par(fig = c(0.005,1,0.1,0.5),new=TRUE)
-plot(NULL,NULL,xlim = c(-1,11),ylim = c(0,max(1/c(mean(null300.RT),mean(null150.RT),mean(decoy300.RT),mean(decoy150.RT)))),xaxt='n',xlab='',ylab='',main = expression('Apparent Dissociation Rates: TREX1 + [Cy5]d(N)'[5]*' + d(N)'[5]),axes=FALSE,cex.main=2)
-axis(side = 2,at = c(0:3)*1e-2,cex.axis=1.2)
-mtext(expression('k'['off']*' (s'^-1*')'),side = 2,line = 2.5,at = 1.5e-2,cex = 1.5)
-axis(side = 1,at = c(0,10),labels = c(0,10),cex.axis=1.25)
-mtext(paste0('[Decoy] (µM)'),side = 1,line = 2.5,at = 5,cex = 1.5)
-points(c(0,10),c(mean(1/c(mean(null300.RT),mean(null150.RT))),mean(1/c(mean(decoy300.RT),mean(decoy150.RT)))),col=c('blue','red'),pch = '-',cex = 5)
-arrows(x0 = c(0,10),x1 = c(0,10),y0 = c(min(1/c(mean(null300.RT),mean(null150.RT))),min(1/c(mean(decoy300.RT),mean(decoy150.RT)))), y1 = c(max(1/c(mean(null300.RT),mean(null150.RT))),max(1/c(mean(decoy300.RT),mean(decoy150.RT)))),code = 3,col = c('blue','red'),lwd = 1,angle = 90,length = 0.5)
-temp.1=signif(mean(1/c(mean(null300.RT),mean(null150.RT))),2)
-temp.2=signif((mean(1/c(mean(decoy300.RT),mean(decoy150.RT)))-mean(1/c(mean(null300.RT),mean(null150.RT))))/10e-6,2)
-mtext(substitute(paste('k'[-1],' = ',temp.1,' (s'^-1,')'),list(temp.1 = temp.1)),side = 1,line = 6,at = 2.5,cex = 2)
-mtext(substitute(paste('k'[theta],' = ',temp.2,' (M'^-1,'s'^-1,')'),list(temp.2 = temp.2)),side = 1,line = 6,at = 7.5,cex = 2)
+mtext(paste0(rep(rep(c('A - ','B - '),each=2),times=2),rep(c('0.2s','0.5s'),times=4)),side = 1,line = rev(-1*seq(1.2,6,(6-1.2)/7)),at = -5,cex = 0.8,col = rep(c('blue','red'),each=4))
+
+
+par(fig = c(0.405,1,0.65,1),new=TRUE)
+plot(NULL,NULL,xlim = c(-1,11),ylim = c(0,max(1/c(mean(e1.10uM.200),mean(e1.10uM.500),mean(e2.10uM.200),mean(e2.10uM.500)))),xaxt='n',xlab='',ylab='',main = expression('TREX1 + [Cy5]d(N)'[5]*' + d(N)'[5]),axes=FALSE,cex.main=2)
+axis(side = 2,at = seq(0,20,5)*1e-2,cex.axis=1.5)
+mtext(expression('k'['off']*''^obs*' (s'^-1*')'),side = 2,line = 2.5,at = 1e-1,cex = 2)
+axis(side = 1,at = c(0,10),labels = c(0,10),cex.axis=1.5)
+mtext(paste0('[Competitor] (µM)'),side = 1,line = 2.5,at = 5,cex = 1.6)
+points(c(0,10),c(fit.0$estimate,fit.10$estimate),col=c('blue','red'),pch = '-',cex = 5)
+arrows(x0 = c(0,10),x1 = c(0,10),y0 = c(fit.0$estimate-fit.0sd,fit.10$estimate-fit.10sd), y1 = c(fit.0$estimate+fit.0sd,fit.10$estimate+fit.10sd),code = 3,col = c('blue','red'),lwd = 1,angle = 90,length = 0.5)
+temp.1=signif(fit.0$estimate,2)
+temp.2=signif(diff(c(fit.0$estimate,fit.10$estimate))/10e-6,2)
+mtext(substitute(paste('k'[-1],' = ',temp.1,' (s'^-1,')'),list(temp.1 = temp.1)),side = 1,line = 6,at = 1.5,cex = 1.6)
+mtext(substitute(paste('k'[theta],' = ',temp.2,' (M'^-1,'s'^-1,')'),list(temp.2 = temp.2)),side = 1,line = 6,at = 8.5,cex = 1.6)
 
